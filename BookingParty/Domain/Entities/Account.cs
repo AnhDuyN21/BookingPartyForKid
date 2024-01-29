2ace79fd94c187d4a6e78b43db4af2205102be47
Nguyễn Anh Duy <duynguyenbt21093@gmail.com>
2024-01-29 13:52:06 +0700

    Create Project

---
+++ BookingParty/Domain/Entities/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Account:BaseEntity
+    {
+        public string FullName { get; set; }
+        public string? Email { get; set; }
+        public string? Gender { get; set; }
+        public string? PasswordHash { get; set; }
+        public bool Status { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Role { get; set; }
+        public string? ConfirmationToken { get; set; }
+        public bool IsConfirmed { get; set; }
+    }
+}
