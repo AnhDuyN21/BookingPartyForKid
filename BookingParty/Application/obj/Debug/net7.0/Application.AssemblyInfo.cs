e0783cad17772c28759b79120a3dc031d9601f29
haocetu <nhathao230502@gmail.com>
2024-02-29 16:30:49 +0700

    conflict test

--- BookingParty/Application/obj/Debug/net7.0/Application.AssemblyInfo.cs
+++ BookingParty/Application/obj/Debug/net7.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+619744ede6a9958e81f8485b25406aacd0faeae1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0909768cc80543e40b42e69d1182da3e45fe20e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
